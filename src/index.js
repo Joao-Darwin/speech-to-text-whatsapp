c10b559a09deaaa6e3e719a009855a8d2781c685
João Darwin <joaodarwin.ip22@gmail.com>
2023-11-03 09:24:28 -0300

    feat: Adicionar a captura do /toText

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const qrCode = require("qrcode-terminal");
 const { Client, LocalAuth } = require('whatsapp-web.js');
+const audioToText = require('./openai');
 
 const client = new Client({
     authStrategy: new LocalAuth()
@@ -14,8 +15,27 @@ client.on('ready', () => {
     console.log('Client is connected!');
 });
 
-client.on('message', msg => {
-    console.log(msg.body);
+client.on('message', async msg => {
+
+    if(await isAudioMessageAndNotGroup(msg)) {
+        const mediaMessage = await msg.downloadMedia();
+        console.log(mediaMessage.data);
+    }
+
+});
+
+const isAudioMessageAndNotGroup = async (msg) => {
+    return msg.hasMedia && !(await msg.getChat()).isGroup && msg.type == 'AUDIO';
+}
+
+client.on('message_create', async msg => {
+
+    if(msg.body == '/toText') {
+        // const textAudio = await audioToText("audio.ogg");
+        await msg.reply(`*Bot:* ${"textAudio"}`);
+        await msg.delete(true);
+    }
+
 });
 
 client.initialize();
