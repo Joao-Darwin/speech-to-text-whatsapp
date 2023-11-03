16584810dd903082395f878a54567f08d61535b5
João Darwin <joaodarwin.ip22@gmail.com>
2023-11-03 14:51:59 -0300

    feat: Implementar a save do audio ao ser recebido

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const fs = require('fs');
 const clientWhatsapp = require("./clientWhatsapp/index");
 const audioToText = require('./openai');
 
@@ -6,12 +7,30 @@ clientWhatsapp.on('message', async msg => {
 
     if (await isAudioMessageAndNotGroup(msg)) {
         const mediaMessage = await msg.downloadMedia();
+
+        let fileName = mediaMessage.filename ? mediaMessage.filename : "audio";
+
+        convertBase64ToAudio(mediaMessage.data, fileName);
     }
 
 });
 
 const isAudioMessageAndNotGroup = async (msg) => {
     return msg.hasMedia && !(await msg.getChat()).isGroup && (msg.type == 'audio' || msg.type == 'ptt');
+}
+
+const convertBase64ToAudio = (base64Content, fileName) => {
+
+    const binaryData = Buffer.from(base64Content, 'base64');
+    const outputPath = `${__dirname}/resources/${fileName}.mp3`;
+
+    fs.writeFile(outputPath, binaryData, 'binary', (err) => {
+        if (err) {
+            console.error("Erro ao salvar o arquivo de áudio:", err);
+        } else {
+            console.log("Arquivo de áudio salvo com sucesso em " + outputPath);
+        }
+    });
 }
 
 clientWhatsapp.on('message_create', async msg => {
